5dec5c13ea2c4b5f0d45a2666018714f3e7efb5d
John Zhou <john.zhou1@ucalgary.ca>
2025-10-08 08:04:40 -0600

    assignment 4

--- quiz_correction.cpp
+++ quiz_correction.cpp
@@ -6,9 +6,22 @@ template <class T, class D>
 void printValue(T value) {
     cout << "Value: " << value << endl;
 }
+template <>
+void printValue<int,double>(int value){
+    cout << "Specialized int-double: " << value << endl;
+}
+template <>
+void printValue<double,double>(double value){
+    cout << "Specialized d-d: " << value << endl;
+}
 
+template <>
+void printValue<double,int>(double value){
+    cout << "Specialized d-int: " << value << endl;
+}
 int main() {
     printValue<int, double>(42);   // D = double, but not used
-    printValue<string, char>("Hello"); // D = char, not used
+    printValue<double, double>(423.33); // D = char, not used
+        printValue<double, int>(43.33); // D = char, not used
     return 0;
 }
